a2bb6ec6316a151da669c172ab04d7ee9879a567
Milena Alexieva <doycheva_m@abv.bg>
2021-05-27 13:38:20 +0300

    Add files via upload

---
+++ EntityFrameWorkIntro/4.Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dbContext = new SoftUniContext();
+            //string res = GetEmployeesFullInformation(dbContext);
+            string res = GetEmployeesWithSalaryOver50000(dbContext);
+            Console.WriteLine(res);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+            {
+                x.FirstName,
+                x.Salary
+            });
+            StringBuilder sb = new StringBuilder();
+            foreach (var empl in employees.Where(x=>x.Salary>50000).OrderBy(y=>y.FirstName).ToList())
+            {
+                sb.AppendLine($"{empl.FirstName} - {empl.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employee = context.Employees.Select(
+                x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.JobTitle,
+                    x.Salary
+                });
+            StringBuilder sb = new StringBuilder();
+            foreach (var empl in employee.OrderBy(x => x.EmployeeId).ToList())
+            {
+                sb.AppendLine($"{empl.FirstName} {empl.LastName} {empl.MiddleName} {empl.JobTitle} {empl.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
